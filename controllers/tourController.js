a90f40c5f3e4dadaeecc9755a699cc2766e0c157
Bryan M <allen.bry95@gmail.com>
2020-12-02 14:01:07 +0100

    Aliasing

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,14 @@
 /* eslint-disable prettier/prettier */
 const Tour = require('../models/tourModel');
 
+exports.aliasTopTours = (req, res, next) => {
+  // Prefilling fields
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,difficulty';
+  next();
+};
+
 exports.getAllTours = async (req, res) => {
   try {
     // console.log(req.query);
