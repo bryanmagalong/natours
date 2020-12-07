21f3ef5dc553343a814f68622200ef3e3ac79add
Bryan M <allen.bry95@gmail.com>
2020-12-07 11:50:48 +0100

    Errors during development and production

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,39 @@
+/* eslint-disable prettier/prettier */
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  }
+  else {
+    console.error('!!!!! ERROR !!!!!: ', err);
+
+    res.status(err.statusCode).json({
+      status: 'error',
+      message: 'An error has occured!!',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500; // default status code
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  }
+
+  if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
