1cabb4f5a42d2187e8b3ed5900eb78c19c2afc68
Bryan M <allen.bry95@gmail.com>
2021-02-01 17:17:56 +0100

    Updating the current user: password

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 const express = require('express');
 const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
@@ -9,6 +10,12 @@ router.post('/login', authController.login);
 
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
+
+router.patch(
+  '/updateMyPassword',
+  authController.protect,
+  authController.updatePassword,
+);
 
 router
   .route('/')
