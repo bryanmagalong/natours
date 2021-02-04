2fdbf48f7ddae0208eb1112c0106ba42ad083a27
Bryan M <allen.bry95@gmail.com>
2021-02-04 14:14:24 +0100

    Modelling Locations (Geospatial Data)

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -23,9 +23,7 @@ mongoose
   .then(() => console.log('DB connection successful!!!'));
 
 // READ JSON FILE
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'),
-);
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
 
 // IMPORT DATA INT DB
 const importData = async () => {
--- models/tourModel.js
+++ models/tourModel.js
@@ -92,6 +92,29 @@ const tourSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    startLocation: {
+      //GeoJSON
+      type: {
+        type: String,
+        default: 'Point',
+        enum: [ 'Point' ],
+      },
+      coordinates: [ Number ],
+      address: String,
+      description: String,
+    },
+    locations: [
+      {
+        type: {
+          type: String,
+          default: 'Point',
+          enum: [ 'Point' ],
+        },
+        address: String,
+        description: String,
+        coordinates: [ Number ],
+      },
+    ],
   },
   {
     toJSON: { virtuals: true },
