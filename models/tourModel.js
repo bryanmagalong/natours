a18c6f7691ff874d85172c35d4aae6c93a9c41cd
Bryan M <allen.bry95@gmail.com>
2021-02-04 14:25:33 +0100

    Modelling Tour Guides: embedding

--- models/tourModel.js
+++ models/tourModel.js
@@ -2,6 +2,7 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 // const validator = require('validator');
+const User = require('./userModel');
 
 // Tour schema
 const tourSchema = new mongoose.Schema(
@@ -110,11 +111,13 @@ const tourSchema = new mongoose.Schema(
           default: 'Point',
           enum: [ 'Point' ],
         },
+        coordinates: [ Number ],
         address: String,
         description: String,
-        coordinates: [ Number ],
+        day: Number,
       },
     ],
+    guides: Array,
   },
   {
     toJSON: { virtuals: true },
@@ -129,6 +132,12 @@ tourSchema.virtual('durationWeeks').get(function() {
 //==== DOCUMENT MIDDLEWARE
 tourSchema.pre('save', function(next) {
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+tourSchema.pre('save', async function(next) {
+  const guidesPromises = this.guides.map(async (id) => await User.findById(id)); // Array of Promises
+  this.guides = await Promise.all(guidesPromises);
   next();
 });
 
